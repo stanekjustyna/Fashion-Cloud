1f248a7c19af3e41dac1a5398b3047b6e51208e1
stanekjustyna <justynka.stanek.js@gmail.com>
2020-02-26 18:12:40 +0100

    first commit

---
+++ src/main/java/js/fashioncloud/FashionCloudApplication.java
@@ -0,0 +1,13 @@
+package js.fashioncloud;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FashionCloudApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FashionCloudApplication.class, args);
+	}
+
+}
