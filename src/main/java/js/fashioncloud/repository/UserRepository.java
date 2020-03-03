e57f90ccf93f403cd2d59105dd270b06181e7321
stanekjustyna <justynka.stanek.js@gmail.com>
2020-03-03 22:51:02 +0100

    add spring security

---
+++ src/main/java/js/fashioncloud/repository/UserRepository.java
@@ -0,0 +1,11 @@
+package js.fashioncloud.repository;
+
+import js.fashioncloud.model.User;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface UserRepository extends JpaRepository<User, Long> {
+
+    User findUserByUsername(String username);
+}
---
+++ src/main/java/js/fashioncloud/service/UserDetailsServiceImpl.java
@@ -0,0 +1,31 @@
+package js.fashioncloud.service;
+
+import js.fashioncloud.model.User;
+import js.fashioncloud.repository.UserRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.stereotype.Service;
+
+@Service
+public class UserDetailsServiceImpl implements UserDetailsService {
+
+    private UserRepository userRepository;
+
+    @Autowired
+    public UserDetailsServiceImpl(UserRepository userRepository) {
+        this.userRepository = userRepository;
+    }
+
+    @Override
+    public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
+        User user = userRepository.findUserByUsername(username);
+
+        if(user != null){
+            return user;
+        }
+
+        throw new UsernameNotFoundException("User '" + username + "' not found.");
+    }
+}
