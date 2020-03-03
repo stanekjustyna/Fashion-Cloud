92056653de44b8ed7efd40cb3d11ec1da03a1e90
stanekjustyna <justynka.stanek.js@gmail.com>
2020-03-03 18:27:10 +0100

    in-memory user store

--- src/main/java/js/fashioncloud/configuration/security/SecurityConfig.java
+++ src/main/java/js/fashioncloud/configuration/security/SecurityConfig.java
@@ -4,12 +4,28 @@ import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.crypto.factory.PasswordEncoderFactories;
+import org.springframework.security.crypto.password.PasswordEncoder;
 
 @Configuration
 @EnableWebSecurity
 public class SecurityConfig extends WebSecurityConfigurerAdapter {
+
     @Override
     protected void configure(AuthenticationManagerBuilder auth) throws Exception {
-        
+
+        PasswordEncoder encoder = PasswordEncoderFactories.createDelegatingPasswordEncoder();
+
+        auth
+                .inMemoryAuthentication()
+                .withUser("user1")
+                .password(encoder.encode("user1"))
+                .authorities("ROLE_USER")
+
+                .and()
+
+                .withUser("user2")
+                .password(encoder.encode("user2"))
+                .authorities("ROLE_USER");
     }
 }
